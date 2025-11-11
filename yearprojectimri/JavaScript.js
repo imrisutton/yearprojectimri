d0244b0425c396a3cc2d9cd3f0b9d4eb52162f28
imri <imri.sutton.ps@gmail.com>
2025-11-11 21:10:04 +0200

    new js

--- yearprojectimri/JavaScript.js
+++ yearprojectimri/JavaScript.js
@@ -1,1 +1,19 @@
-﻿alert(heli);
+﻿var name = prompt("הכנס את שמך", "");
+document.write("<br/>")
+document.write("hello " + name);
+document.write("<br/>")
+document.write("hello yoav ");
+document.write("<br/>")
+var age = parseInt(prompt("הכנס את גילך", 0));  
+document.write("<br/>")
+document.write("שנה הבאה אתה תהיה בן " + (age + 1));
+document.write("<br/>")
+var n1 = parseInt(prompt("הכנס מספר", 0));  
+var n2 = parseInt(prompt("הכנס מספר", 0));
+document.write(n1 + "+" + n2 +"="+ (n1 + n2))
+document.write("<br/>")
+document.write(n1 + "-" + n2 +"="+ (n1 - n2))
+document.write("<br/>")
+document.write(n1 + "*" + n2 +"="+ (n1 * n2))
+document.write("<br/>")
+document.write(n1 + "/" + n2 +"="+ (n1 / n2))
